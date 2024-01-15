1708cc027f248234434a586e81d157e7a8001437
Radhika-222 <wandareradhika@gmail.com>
2024-01-15 23:24:52 +0530

    Ph4AssignmentDay7

---
+++ WebAppAssignment_Day7/WebAppAssignment_Day7/Controllers/PlayersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppAssignment_Day7.Models;
+
+namespace WebAppAssignment_Day7.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private List<Player> players = new List<Player>()
+        {
+            new Player(){PId=1,PName="MS.Dhoni",PCountry="India",PType="WicketKeeper"},
+            new Player(){PId=2,PName="Rohit Sharma",PCountry="India",PType="Batsman"},
+            new Player(){PId=3,PName="R.Jadeja",PCountry="India",PType="All-rounder"}
+
+        };
+        public IActionResult Index()
+        {
+            ViewBag.Countries = GetDistinctCountries();
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        [HttpPost]
+        public IActionResult Create(Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                players.Add(player);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        private IEnumerable<string> GetDistinctCountries()
+        {
+            return players.Select(players => players.PCountry).Distinct();
+        }
+
+
+    }
+}
---
+++ WebAppAssignment_Day7/WebAppAssignment_Day7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppAssignment_Day7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
